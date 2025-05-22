980fdc3f085c3eeaff9d5bf553895f97a7078593
Elyahya01 <yahyayuri10@gmail.com>
2025-05-22 19:37:06 +0100

    program that copies its input to the output part 1 of exercice1-9

---
+++ exercice1-9.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+// copy input to output and replace each string of blanks with a single blank
+
+int main () 
+{
+    int c;
+   
+    while ((c = getchar()) != EOF)
+	    putchar(c);
+    
+    return 0; 
+}
