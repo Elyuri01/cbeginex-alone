dc4a57d58824b8e2969a2a17f0f6cb20f95af7ff
Elyahya01 <yahyayuri10@gmail.com>
2025-05-24 13:12:46 +0100

    ex1-9 but took a lot of time its worth it learnt new technique memory flags

--- exercice1-9.c
+++ exercice1-9.c
@@ -1,13 +1,31 @@
 # include <stdio.h>
-
+ //lastcbl means last character is a blank
 // copy input to output and replace each string of blanks with a single blank
 
-int main () 
+int main ()
 {
     int c;
-   
-    while ((c = getchar()) != EOF)
-	    putchar(c);
+    int lastcbl = 0;
     
-    return 0; 
+    while ((c = getchar()) != EOF) {
+	    if (c == ' ') {
+		    if (!lastcbl) {
+			    putchar(' ');
+			    lastcbl = 1;
+		    }
+			  
+		   
+	   
+
+	    }
+	    else {
+		    putchar(c);
+		    lastcbl = 0;
+	    }
+    }
+
+	     
+   
+    return 0;
 }
+	    
